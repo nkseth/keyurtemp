91a6c71cee1b2b00487fe9fa0136d9532b851e26
mohsin <l5nr7fl2lfsaaepdp44spkuhj2huzuksja3lfsuerolajq56sb6a>
2021-10-30 12:27:01 +0400

    config fix

--- src/config.js
+++ src/config.js
@@ -19,11 +19,12 @@ export const auth0Config = {
   domain: process.env.REACT_APP_AUTH0_DOMAIN
 };
 
-export default {
-	REACT_APP_MAGIC_PUBLIC_KEY: process.env.REACT_APP_MAGIC_PUBLIC_KEY,
-	REACT_APP_ENDPOINT: process.env.REACT_APP_ENDPOINT,
-	REACT_APP_CAPTCHA: process.env.REACT_APP_CAPTCHA,
-};
+//We dont need this. We got these available in /constants/config.js
+// export default {
+// 	REACT_APP_MAGIC_PUBLIC_KEY: process.env.REACT_APP_MAGIC_PUBLIC_KEY,
+// 	REACT_APP_ENDPOINT: process.env.REACT_APP_ENDPOINT,
+// 	REACT_APP_CAPTCHA: process.env.REACT_APP_CAPTCHA,
+// };
 
 export const mapConfig = process.env.REACT_APP_MAP_MAPBOX;
 export const googleAnalyticsConfig = process.env.REACT_APP_GA_MEASUREMENT_ID;
